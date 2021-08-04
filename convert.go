3eef4519393758b5626e2f7ac292613d2699239b
Fabio Forni <fabio@redaril.me>
2021-08-04 11:39:20 +0200

    scalarToString: Support TextMarshaler interface

--- convert.go
+++ convert.go
@@ -1,41 +1,39 @@
 package redmap
 
 import (
+	"encoding"
 	"fmt"
 	"reflect"
 	"strconv"
 )
 
-func scalarToString(val reflect.Value) string {
+func scalartoString(val reflect.Value) (string, error) {
 	switch val.Kind() {
-	case reflect.Int,
-		reflect.Int8,
-		reflect.Int16,
-		reflect.Int32,
-		reflect.Int64:
-		return strconv.FormatInt(val.Int(), 10)
-	case reflect.Uint,
-		reflect.Uint8,
-		reflect.Uint16,
-		reflect.Uint32,
-		reflect.Uint64,
-		reflect.Uintptr:
-		return strconv.FormatUint(val.Uint(), 10)
-	case reflect.Float32:
-		return strconv.FormatFloat(val.Float(), 'f', -1, 32)
-	case reflect.Float64:
-		return strconv.FormatFloat(val.Float(), 'f', -1, 64)
-	case reflect.Complex64:
-		return strconv.FormatComplex(val.Complex(), 'f', -1, 64)
-	case reflect.Complex128:
-		return strconv.FormatComplex(val.Complex(), 'f', -1, 128)
-	case reflect.String:
-		return val.String()
 	case reflect.Bool:
-		return strconv.FormatBool(val.Bool())
+		return strconv.FormatBool(val.Bool()), nil
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return strconv.FormatInt(val.Int(), 10), nil
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return strconv.FormatUint(val.Uint(), 10), nil
+	case reflect.Float32:
+		return strconv.FormatFloat(val.Float(), 'f', -1, 32), nil
+	case reflect.Float64:
+		return strconv.FormatFloat(val.Float(), 'f', -1, 64), nil
+	case reflect.Complex64:
+		return strconv.FormatComplex(val.Complex(), 'f', -1, 64), nil
+	case reflect.Complex128:
+		return strconv.FormatComplex(val.Complex(), 'f', -1, 128), nil
+	case reflect.String:
+		return val.String(), nil
+	case reflect.Struct:
+		typ := val.Type()
+		if typ.Implements(reflect.TypeOf(encoding.TextMarshaler(nil))) {
+			str, err := val.Interface().(encoding.TextMarshaler).MarshalText()
+			return string(str), err
+		}
+		if typ.Implements(reflect.TypeOf(fmt.Stringer(nil))) {
+			return val.Interface().(fmt.Stringer).String(), nil
+		}
 	}
-	if val.Type().Implements(reflect.TypeOf(fmt.Stringer(nil))) {
-		return val.Interface().(fmt.Stringer).String()
-	}
-	panic(fmt.Sprintf("%s is not a scalar value", val.Type().String()))
+	panic(fmt.Sprintf("%s doesn't implement TextMarshaler or Stringer", val.Type().String()))
 }
