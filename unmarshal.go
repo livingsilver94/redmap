e4b542926b35851e7f0e7e93443a4f0166127767
Fabio Forni <fabio@redaril.me>
2021-09-29 20:54:03 +0200

    Add initial support to Unmarshaling

--- unmarshal.go
+++ unmarshal.go
@@ -1,7 +1,10 @@
 package redmap
 
 import (
+	"encoding"
+	"fmt"
 	"reflect"
+	"strconv"
 )
 
 func Unmarshal(data map[string]string, v interface{}) error {
@@ -42,6 +45,115 @@ func ptrStructValue(v interface{}) (reflect.Value, error) {
 	}
 }
 
-func unmarshalRecursive(data map[string]string, val reflect.Value) error {
+func unmarshalRecursive(data map[string]string, stru reflect.Value) error {
+	typ := stru.Type()
+	for i := 0; i < typ.NumField(); i++ {
+		field := typ.Field(i)
+		tags := redmapTags(field.Tag)
+		value := stru.Field(i)
+		if tags.ignored {
+			continue
+		}
+		if tags.name == "" {
+			tags.name = field.Name
+		}
+
+		for value.Kind() == reflect.Ptr {
+			value = value.Elem()
+		}
+		if !value.CanSet() {
+			continue
+		}
+
+		if tags.inline {
+			if kind := value.Kind(); kind != reflect.Struct {
+				return fmt.Errorf("cannot inline: %w", errIs(value.Type(), ErrNotStruct))
+			}
+			//err := marshalRecurse(mp, prefix+tags.name+inlineSep, value)
+			//if err != nil {
+			//	return err
+			//}
+		} else {
+			str, ok := data[tags.name]
+			if !ok {
+				continue
+			}
+			err := stringToField(str, value, tags.omitempty)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func stringToField(str string, field reflect.Value, omitempty bool) error {
+	var (
+		val reflect.Value
+		err error
+	)
+	switch field.Kind() {
+	case reflect.Bool:
+		v, e := strconv.ParseBool(str)
+		val, err = reflect.ValueOf(v), e
+	case reflect.Int:
+		v, e := strconv.ParseInt(str, 10, 0)
+		val, err = reflect.ValueOf(int(v)), e
+	case reflect.Int8:
+		v, e := strconv.ParseInt(str, 10, 8)
+		val, err = reflect.ValueOf(int8(v)), e
+	case reflect.Int16:
+		v, e := strconv.ParseInt(str, 10, 16)
+		val, err = reflect.ValueOf(int16(v)), e
+	case reflect.Int32:
+		v, e := strconv.ParseInt(str, 10, 32)
+		val, err = reflect.ValueOf(int32(v)), e
+	case reflect.Int64:
+		v, e := strconv.ParseInt(str, 10, 64)
+		val, err = reflect.ValueOf(v), e
+	case reflect.Uint:
+		v, e := strconv.ParseUint(str, 10, 0)
+		val, err = reflect.ValueOf(uint(v)), e
+	case reflect.Uint8:
+		v, e := strconv.ParseUint(str, 10, 8)
+		val, err = reflect.ValueOf(uint8(v)), e
+	case reflect.Uint16:
+		v, e := strconv.ParseUint(str, 10, 16)
+		val, err = reflect.ValueOf(uint16(v)), e
+	case reflect.Uint32:
+		v, e := strconv.ParseUint(str, 10, 32)
+		val, err = reflect.ValueOf(uint32(v)), e
+	case reflect.Uint64:
+		v, e := strconv.ParseUint(str, 10, 64)
+		val, err = reflect.ValueOf(v), e
+	case reflect.Float32:
+		v, e := strconv.ParseFloat(str, 32)
+		val, err = reflect.ValueOf(float32(v)), e
+	case reflect.Float64:
+		v, e := strconv.ParseFloat(str, 64)
+		val, err = reflect.ValueOf(v), e
+	case reflect.Complex64:
+		v, e := strconv.ParseComplex(str, 64)
+		val, err = reflect.ValueOf(complex64(v)), e
+	case reflect.Complex128:
+		v, e := strconv.ParseComplex(str, 128)
+		val, err = reflect.ValueOf(v), e
+	case reflect.String:
+		val, err = reflect.ValueOf(str), nil
+	case reflect.Struct:
+		f := field.Addr() // Unmarshaling always requires a pointer receiver.
+		t := f.Type()
+		if t.Implements(reflect.TypeOf((*encoding.TextUnmarshaler)(nil)).Elem()) {
+			return f.Interface().(encoding.TextUnmarshaler).UnmarshalText([]byte(str))
+		}
+		return fmt.Errorf("%s doesn't implement TextUnmarshaler", f.Type())
+	}
+	if err != nil {
+		return err
+	}
+	if omitempty && val.IsZero() {
+		return nil
+	}
+	field.Set(val)
 	return nil
 }
--- unmarshal_test.go
+++ unmarshal_test.go
@@ -9,6 +9,13 @@ import (
 	"github.com/livingsilver94/redmap"
 )
 
+type StubTextUnmarshaler struct{ S string }
+
+func (s *StubTextUnmarshaler) UnmarshalText(text []byte) error {
+	s.S = string(text)
+	return nil
+}
+
 var emptyMap = make(map[string]string)
 
 func TestUnmarshalValid(t *testing.T) {
@@ -69,6 +76,41 @@ func TestUnmarshalInvalid(t *testing.T) {
 		err := redmap.Unmarshal(emptyMap, test.val().Interface())
 		if !errors.Is(err, test.expErr) {
 			t.Fatalf("Unmarshal returned %q but should have returned %q", err, test.expErr)
+		}
+	}
+}
+
+func TestUnmarshalScalars(t *testing.T) {
+	tests := []struct {
+		In  map[string]string
+		Out interface{}
+	}{
+		{In: map[string]string{"V": "true"}, Out: struct{ V bool }{true}},
+		{In: map[string]string{"V": "100"}, Out: struct{ V int }{100}},
+		{In: map[string]string{"V": "100"}, Out: struct{ V int8 }{100}},
+		{In: map[string]string{"V": "100"}, Out: struct{ V int16 }{100}},
+		{In: map[string]string{"V": "100"}, Out: struct{ V int32 }{100}},
+		{In: map[string]string{"V": "100"}, Out: struct{ V int64 }{100}},
+		{In: map[string]string{"V": "100"}, Out: struct{ V uint }{100}},
+		{In: map[string]string{"V": "100"}, Out: struct{ V uint8 }{100}},
+		{In: map[string]string{"V": "100"}, Out: struct{ V uint16 }{100}},
+		{In: map[string]string{"V": "100"}, Out: struct{ V uint32 }{100}},
+		{In: map[string]string{"V": "100"}, Out: struct{ V uint64 }{100}},
+		{In: map[string]string{"V": "100.1"}, Out: struct{ V float32 }{100.1}},
+		{In: map[string]string{"V": "100.1"}, Out: struct{ V float64 }{100.1}},
+		{In: map[string]string{"V": "(100.1+80.1i)"}, Out: struct{ V complex64 }{100.1 + 80.1i}},
+		{In: map[string]string{"V": "(100.1+80.1i)"}, Out: struct{ V complex128 }{100.1 + 80.1i}},
+		{In: map[string]string{"V": "str"}, Out: struct{ V string }{"str"}},
+		{In: map[string]string{"V": "a test"}, Out: struct{ V StubTextUnmarshaler }{StubTextUnmarshaler{S: "a test"}}},
+	}
+	for _, test := range tests {
+		zero := reflect.New(reflect.TypeOf(test.Out))
+		err := redmap.Unmarshal(test.In, zero.Interface())
+		if err != nil {
+			t.Fatalf("Unmarshal returned unexpected error %q", err)
+		}
+		if !reflect.DeepEqual(zero.Elem().Interface(), test.Out) {
+			t.Fatalf("Unmarshal's output doesn't match the expected value\n\tIn: %v\n\tExpected: %v\n\tOut: %v", test.In, test.Out, zero)
 		}
 	}
 }
