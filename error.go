50c36faad4dc5daed3ba4bf48bb8fdcb6abe6365
Fabio Forni <fabio@redaril.me>
2021-10-09 16:00:11 +0200

    Document error.go

--- error.go
+++ error.go
@@ -6,9 +6,12 @@ import (
 )
 
 var (
-	ErrNilValue   = errors.New("nil")
+	// ErrNilValue is returned when an argument passed is nil but it should not be.
+	ErrNilValue = errors.New("nil")
+	// ErrNotPointer is retuned when an argument passed is not a pointer but it should be.
 	ErrNotPointer = errors.New("not a pointer")
-	ErrNotStruct  = errors.New("not a struct type")
+	// ErrNotStruct is returned when an argument passed is not a struct but it should be.
+	ErrNotStruct = errors.New("not a struct type")
 )
 
 func errIs(something interface{}, err error) error {
